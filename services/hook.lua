e5d49ba0de5c0721fb47cd063905837ea7ec9089
! xSync <141618921+xSync-gg@users.noreply.github.com>
2025-12-16 19:25:00 +0700

    Create hook.lua

---
+++ services/hook.lua
@@ -0,0 +1,18 @@
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local OldNamecall
+OldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
+    local method = getnamecallmethod()
+
+    if self == TeleportService and method == "TeleportToPlaceInstance" then
+        local args = {...}
+        local PlaceID = args[1]
+        local JobID   = args[2]
+        local Player  = args[3]
+
+        print("Teleport to ", JobID)
+    end
+
+    return OldNamecall(self, ...)
+end)
